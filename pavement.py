dfa815593e22e452d74012927c80694c7d5a9bf3
Alex Raichev <araichev@users.noreply.github.com>
2017-05-24 15:14:09 +1200

    Updated paver

--- pavement.py
+++ pavement.py
@@ -53,13 +53,15 @@ def unpack():
 
 @task
 @cmdopts([
+    ('server=', 's', 'Server to push to')
     ('local_branch=', 'l', 'Local Git branch to push'),
     ('remote_branch=', 'r', 'Remote Git branch to push to'),
 ])
-def push_gitlab(options):
-    local_branch = options.push_gitlab.local_branch
+def push(options):
+    server = options.push.server
+    local_branch = options.push.local_branch
     try:
-        remote_branch = options.push_gitlab.remote_branch
+        remote_branch = options.push.remote_branch
     except AttributeError:
         remote_branch = local_branch
 
@@ -67,7 +69,8 @@ def push_gitlab(options):
     pack()
     sh('git add -A')
     sh('git commit -am "Clean up"')
-    sh('git push gitlab {!s}:{!s} --follow-tags'.format(local_branch, remote_branch))
+    sh('git push {!s} {!s}:{!s} --follow-tags'.format(
+      server, local_branch, remote_branch))
     # unpack()
     # sh('git add -A')
     # sh('git commit -am "Unclean up"')

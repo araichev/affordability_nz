07d4a54b65c00017f1b0351395d470a3b1bc2b24
Alex Raichev <araichev@users.noreply.github.com>
2017-10-12 14:07:21 +1300

    Refactored some and simplified notebooks. Updated rent data.

--- py/helpers.py
+++ py/helpers.py
@@ -52,66 +52,107 @@ COST_BY_MODE = {
     'transit': 0,
 }
 
-def get_path(region, key=None):
+def get_secret(key, secrets_path=ROOT/'secrets.json'):
+    """
+    Open the JSON file at ``secrets_path``, and return the value
+    corresponding to the given key.
+    """
+    secrets_path = Path(secrets_path)
+    with secrets_path.open() as src:
+        secrets = json.load(src)
+    return secrets[key]
+
+def get_path(key, region=None):
     """
     Return the path (Path object) of the file corresponding to the given
-    region (string) and key (string).
+    key (string) and region (string).
     """
-    path = DATA_DIR/'processed'/region
-    if key is None:
-        return path
-    elif key == 'rental_areas':
-        path /= 'rental_areas.geojson'
-    elif key == 'rental_points':
-        path /= 'rental_points.geojson'
-    elif key == 'rents_csv':
-        path /= 'rents.csv'
-    elif key == 'rents_json':
-        path /= 'rents.json'
-    elif key == 'commutes_walking':
-        path /= 'commutes_walking.csv'
-    elif key == 'commutes_bicycling':
-        path /= 'commutes_bicycling.csv'
-    elif key == 'commutes_driving':
-        path /= 'commutes_driving.csv'
-    elif key == 'commutes_transit':
-        path /= 'commutes_transit.csv'
-    elif key == 'transit_costs':
-        path /= 'transit_costs.csv'
-    elif key == 'commute_costs':
-        path /= 'commute_costs.json'
+    path = DATA_DIR/'processed'
+    error = ValueError('Invalid key-region pair ({!s}, {!s})'.format(
+      key, region))
+    if region is None:
+        if key == 'property_titles':
+            # Special case using collected data
+            path = DATA_DIR/'collected'/'nz-cadastral-titles-jan-10.gpkg'
+        elif key == 'au2001_csv':
+            path /= 'au2001.csv'
+        elif key == 'au2001':
+            path /= 'au2001.geojson'
+        elif key == 'rental_areas_csv':
+            path /= 'rental_areas.csv'
+        elif key == 'rental_areas':
+            path /= 'rental_areas.geojson'
+        elif key == 'rental_points':
+            path /= 'rental_points.geojson'
+        elif key == 'rents':
+            path /= 'rents.csv'
+        else:
+            raise error
     else:
-        raise ValueError('Invalid region-key pair ({!s}, {!s})'.format(
-          region, key))
+        path /= region
+        if key == 'rental_areas':
+            path /= 'rental_areas.geojson'
+        elif key == 'rental_points':
+            path /= 'rental_points.geojson'
+        elif key == 'rents':
+            path /= 'rents.csv'
+        elif key == 'rents_json':
+            path /= 'rents.json'
+        elif key == 'commutes_walking':
+            path /= 'commutes_walking.csv'
+        elif key == 'commutes_bicycling':
+            path /= 'commutes_bicycling.csv'
+        elif key == 'commutes_driving':
+            path /= region/'commutes_driving.csv'
+        elif key == 'commutes_transit':
+            path /= 'commutes_transit.csv'
+        elif key == 'transit_costs':
+            path /= 'transit_costs.csv'
+        elif key == 'commute_costs':
+            path /= 'commute_costs.json'
+        else:
+            raise error
 
     return path
 
-def get_data(region, key):
+def get_data(key, region=None):
     """
-    Return the data corresponding to the given region (string) and key
-    (string).
+    Return the data corresponding to the given key (string) and
+    region (string) and key.
     """
-    path = get_path(region, key)
+    path = get_path(key, region)
     if not path.exists:
-        raise ValueError('Data does not exist for ({!s}, {!s})'.format(
-          region, key))
+        raise ValueError('Data does not exist for key-region pair'
+         ' ({!s}, {!s})'.format(key, region))
 
     s = path.suffix
     if s == '.csv':
         result = pd.read_csv(path, dtype={'au2001': str})
-    elif s == '.geojson':
+    elif s in ['.geojson', '.gpkg']:
         result = gpd.read_file(str(path))
     elif s == '.json':
         with path.open() as src:
             result = json.load(src)
     return result
 
-def nan_to_none(df):
+def get_latest_quarters(n, from_data=True):
     """
-    Replace the NaN values in the given DataFrame with None and return
-    the resulting DataFrame.
+    Return a list of the latest ``n`` (positive integer)
+    rental data quarters as YYYY-MM-DD datestrings sorted
+    chronologically.
+    Each quarter will be of the form YYYY-03-01, YYYY-06-01,
+    YYYY-09-01, or YYYY-12-01.
+    If ``from_data``, then get the latest quarters from the stored
+    rental data.
+    Otherwise, get the latest quarters theoretically possible from
+    today's date.
     """
-    return df.where((pd.notnull(df)), None)
+    if from_data:
+        quarters = get_data('rents')['quarter'].unique()[-n:].tolist()
+    else:
+        quarters = [q.strftime('%Y-%m') + '-01' for q in
+          pd.date_range(end=dt.datetime.now(), freq='Q', periods=n)]
+    return(quarters)
 
 def aggregate_rents(rents, date=None, groupby_cols=('rental_area',
   '#bedrooms')):
@@ -153,23 +194,32 @@ def aggregate_rents(rents, date=None, groupby_cols=('rental_area',
     g = f.groupby(groupby_cols).apply(my_agg).reset_index()
     return g
 
-def get_secret(key, secrets_path=ROOT/'secrets.json'):
+def nan_to_none(df):
     """
-    Open the JSON file at ``secrets_path``, and return the value
-    corresponding to the given key.
+    Replace the NaN values in the given DataFrame with None and return
+    the resulting DataFrame.
     """
-    secrets_path = Path(secrets_path)
-    with secrets_path.open() as src:
-        secrets = json.load(src)
-    return secrets[key]
+    return df.where((pd.notnull(df)), None)
 
-def get_latest_quarters(n):
+def build_json_rents(rents):
     """
-    Return a list of the latest ``n`` (positive integer) rental data
-    quarters as YYYY-MM-DD datestrings sorted chronologically.
-    Each quarter will be of the form YYYY-03-01, YYYY-06-01,
-    YYYY-09-01, or YYYY-12-01.
+    Given a DataFrame of rents of the form output by
+    :func:read_data('rents'), aggregate the rents by rental area
+    and number of bedrooms ('1', '2', '3', or '4'), and return the
+    result as a dictionary of the form
+    rental area -> #bedrooms -> rent geometric mean.
+    Some of the mean rents could be ``None``.
     """
-    return [q.strftime('%Y-%m') + '-01' for q in
-      pd.date_range(end=dt.datetime.now(), freq='Q', periods=n)]
+    f = aggregate_rents(rents)
 
+    # Drop 5+ bedrooms and round to nearest dollar
+    f = f[f['#bedrooms'] != '5+'].copy().round()
+
+    # Replace NaN with None to make JSON-compatible
+    f = nan_to_none(f)
+
+    # Save to dictionary of form rental area -> #bedrooms -> rent geo mean
+    d = {area: dict(g[['#bedrooms', 'rent_geo_mean']].values)
+      for area, g in f.groupby('rental_area')}
+
+    return d
